a17fa3b148863057f9867e415b87c8840e9fb1cd
Nadeera Thilakarathna <nadeerathilakarathna4@gmail.com>
2024-12-02 21:56:24 +0530

    Edited Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // Your code goes here
-        System.out.println("Hello, World!");
+        GUI gui = new GUI();
+        gui.show();
     }
 }
